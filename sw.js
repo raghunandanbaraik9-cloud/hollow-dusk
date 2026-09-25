dddc60abcffca28280cee28a85fbe6ded9704d25
Raghunandan Baraik <raghunandanbaraik9@gmail.com>
2026-09-25 11:48:56 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'hollow-dusk-v1';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(k =>
+    Promise.all(k.filter(n => n !== CACHE).map(n => caches.delete(n)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
